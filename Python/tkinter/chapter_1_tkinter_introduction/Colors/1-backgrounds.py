de1da4153967af1f800b935665107a65766e9680
James Mick <mick.james01@gmail.com>
2023-12-21 12:39:58 -0600

    Added Colors Section

---
+++ Python/tkinter/chapter_1_tkinter_introduction/Colors/1-backgrounds.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+myroot=Tk()
+
+myl1=Label(myroot, text='Check Out My Background', activebackground='#c12ab8', bg='#f39a1c')
+myl1.pack()
+
+myroot.mainloop()
---
+++ Python/tkinter/chapter_1_tkinter_introduction/Colors/3-disabledforeground.py
@@ -0,0 +1,7 @@
+from tkinter import *
+myroot=Tk()
+
+myb1=Button(myroot, text="Python", state = "disabled", disabledforeground="Magenta")
+myb1.pack()
+
+myroot.mainloop()
